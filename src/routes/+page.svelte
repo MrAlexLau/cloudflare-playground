a3bdeebbf9b8cc0b3d37340bce136b8c05a816cf
Alex Lau <laux0083@gmail.com>
2024-04-07 11:16:22 -0500

    Submit request to worker

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let prompt = "Eclipses";
+  let responseBody;
+
+  function requestOptions(opts = {}) {
+    return {
+      method: "POST",
+      body: JSON.stringify({ ...opts })
+    }
+  }
+
+  async function submitPrompt() {
+    responseBody = "Summarizing your topic...";
+
+    const fullBody = await fetch(
+      "https://worker-restless-cake-bbb8.mralexlau.workers.dev",
+      requestOptions({ prompt })
+    ).then((response) => {
+      return response.json();
+    });
+
+    responseBody = fullBody[0].response;
+  }
+</script>
+
+<h2>Concept Visualizer</h2>
+
+<div>
+  <div>
+    What's a concept you would like to learn more about?
+    <input bind:value={prompt} /> <br />
+
+    <a href="#" on:click={submitPrompt}>
+      Go
+    </a>
+  </div>
+
+  <div>
+    {responseBody || ""}
+  </div>
+</div>
+
+<style>
+</style>
+
